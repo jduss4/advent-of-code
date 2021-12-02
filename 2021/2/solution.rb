fb884882aafc26e5e5baed75c1a270a5c33c2562
JessicaVDussault <jessica.dussault@gsa.gov>
2021-12-02 10:12:21 -0600

    yay problem 2 is happier

--- 2021/2/solution.rb
+++ 2021/2/solution.rb
@@ -2,25 +2,40 @@ require_relative "../../solutions.rb"
 
 class Solution < Solutions
 
-  def initialize(dir_path)
-    super
-    @horizontal = 0
-    @depth = 0
-    @aim = 0
-  end
-
   def problem_one
+    set_position
+
     p1_calc_forward
     p1_calc_depth
     @horizontal * @depth
   end
 
   def problem_two
+    set_position
+
     p2_calc_position
     @horizontal * @depth
   end
 
   private
+
+  def direction_sums(direction)
+    counts = @input
+      .select { |row| row.include?(direction) }
+      .map { |item| item.sub(/[A-z]*\s/, "").to_i }
+    counts.sum
+  end
+
+  def p1_calc_depth
+    down = direction_sums("down")
+    up = direction_sums("up")
+    # note: depth is "positive" (10 depth, not -10 depth)
+    @depth = down - up
+  end
+
+  def p1_calc_forward
+    @horizontal = direction_sums("forward")
+  end
 
   def p2_calc_position
     @input.each do |row|
@@ -39,21 +54,9 @@ class Solution < Solutions
     end
   end
 
-  def p1_calc_depth
-    down = direction_sums("down")
-    up = direction_sums("up")
-    # note: depth is "positive" (10 depth, not -10 depth)
-    @depth = down - up
-  end
-
-  def p1_calc_forward
-    @horizontal = direction_sums("forward")
-  end
-
-  def direction_sums(direction)
-    counts = @input
-      .select { |row| row.include?(direction) }
-      .map { |item| item.sub(/[A-z]*\s/, "").to_i }
-    counts.sum
+  def set_position
+    @horizontal = 0
+    @depth = 0
+    @aim = 0
   end
 end
